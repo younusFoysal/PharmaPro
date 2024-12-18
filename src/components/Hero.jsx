8a0c07fd16d69ef0fa8b12e70362c72d6d5b5666
younusFoysal <younus.faysal@gmail.com>
2024-12-18 11:17:22 +0600

    Hero button fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,13 +18,13 @@ const Hero = () => {
                                 </p>
                                 <h1 className="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">
                                     Browse
-                                    & Buy your Medicine.</h1>
+                                    And Buy your Medicine.</h1>
                                 <p className="mt-4 text-base text-black lg:mt-8 sm:text-xl">
                                     Get your treatment fast with right doctor.
                                 </p>
 
                                 <a href="#" title=""
-                                   className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-white hover:text-black transition-all duration-200 bg-green-900 rounded-full lg:mt-16 hover:bg-green-300 shadow-lg focus:bg-yellow-400"
+                                   className="inline-flex items-center px-6 py-4 mt-8 font-semibold hover:scale-105 duration-500 text-white hover:text-black transition-all  bg-green-900 rounded-full lg:mt-16 hover:bg-green-300 shadow-lg focus:bg-yellow-400"
                                    role="button">
                                     Join for free
                                     <svg className="w-6 h-6 ml-8 -mr-2" xmlns="http://www.w3.org/2000/svg" fill="none"
